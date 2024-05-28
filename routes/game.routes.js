b3fe59af775a63ffec97435fb738341cbb442d90
Anilkokkul <anilkokkul1997@gmail.com>
2024-05-28 20:35:36 +0530

    updated

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -10,13 +10,13 @@ router.post("/create-room", async (req, res) => {
   res.status(201).send("Game room created");
 });
 
-router.post("/join-room", async (req, res) => {
-  const { roomId, userId } = req.body;
-  const gameRoom = await GameRoom.findOne({ roomId });
-  if (!gameRoom) return res.status(404).send("Room not found");
-  gameRoom.players.push(userId);
-  await gameRoom.save();
-  res.status(200).send("Joined room");
-});
+// router.post("/join-room", async (req, res) => {
+//   const { roomId, userId } = req.body;
+//   const gameRoom = await GameRoom.findOne({ roomId });
+//   if (!gameRoom) return res.status(404).send("Room not found");
+//   gameRoom.players.push(userId);
+//   await gameRoom.save();
+//   res.status(200).send("Joined room");
+// });
 
 module.exports = router;
